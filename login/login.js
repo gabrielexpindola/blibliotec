5988d0c75b37ae4bc52770fd975ae58a22d5bccf
kessiamilena <kessia.milena124@yahoo.com>
2026-09-08 19:34:14 -0300

    :construction: build: Desenvolvimento em andamento da tela de listagem de livros: menu lateral criado.

--- login/login.js
+++ login/login.js
@@ -19,8 +19,18 @@
 const campoSenha = document.getElementById("senha");
 const btnSenha = document.querySelector("#mostrar-senha");
 
+console.log(btnSenha.type)
+
 btnSenha.addEventListener("click", function() {
 
-    alert("Cliquei no botão!");
+    // if(campoSenha.type == "password") {
+
+    //     campoSenha.type = "text";
+    // }
+    // else {
+    //     campoSenha.type = "password";
+    // }
+
+    campoSenha.type = campoSenha.type == "password" ? "text" : "password";
     
 })
