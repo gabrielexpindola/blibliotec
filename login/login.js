19d34d8c5cfa52a0b189c71e082a39213fcc890b
kessiamilena <kessia.milena124@yahoo.com>
2026-09-08 14:17:54 -0300

    :construction: build: Página de login em andamento: criação da tela e estilização, acrescentando funcionalidades em javascript.

---
+++ login/login.js
@@ -0,0 +1,26 @@
+
+// var nome = "Késsia"
+// const nome = "Késsia"
+
+// let nome = "Késsia";
+
+// console.log(nome);
+
+// nome = "Gustavo"
+// console.log(nome);
+
+
+// const nome = "Késsia"
+// console.log(nome);
+
+// nome = "Gustavo"
+// console.log(nome);
+
+const campoSenha = document.getElementById("senha");
+const btnSenha = document.querySelector("#mostrar-senha");
+
+btnSenha.addEventListener("click", function() {
+
+    alert("Cliquei no botão!");
+    
+})
